20c38d1e7ed9f98b9f951a421c9e657e09ae408a
Hugo Zapata <hugo@volarelabs.co>
2021-01-28 20:53:39 -0500

    Add Refresh Diagram Button

--- src/entries/diagram-codes.ts
+++ src/entries/diagram-codes.ts
@@ -2,6 +2,8 @@ import { createButtonObserver, runExtension } from "../entry-helpers";
 import { render } from "../components/DiagramCodes";
 import { addButtonListener, getParentUidByBlockUid,getOrderByBlockUid  } from "roam-client";
 
+import diagramEngine from 'diagram-codes-engine-client'
+diagramEngine.setEnginePath('https://web-engine-demo-dev.diagram.codes/apirender/')
 
 /* Create The Code Block*/
 const createDiagram = async(_:{
@@ -30,9 +32,20 @@ blockUid: string
     },
   });
 
+  window.roamAlphaAPI.createBlock({
+    block: {
+      string: "{{Refresh Diagram}}",
+    },
+    location: {
+      "parent-uid": blockUid,
+      order: getOrderByBlockUid(blockUid),
+    },
+  });
+
   
 }
 runExtension("diagram-codes", () => {
     console.log('prueba extension diagram codes')
+    diagramEngine.setEnginePath('https://web-engine-demo-dev.diagram.codes/apirender/')
     addButtonListener("Add Diagram", createDiagram)
 });
